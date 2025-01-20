e1e5481bbcc0e1d4bb2122f71e8307165915c4e6
Ayman <aymanfouad033@gmail.com>
2025-01-19 20:50:39 -0500

    Codeview UI implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
+import { codeSnippets } from './codeSnippets'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import PrettyPrintToggle from './components/prettyPrintToggle'
 function App() {
-  const [count, setCount] = useState(0)
+  const languages = ['javascript', 'python', 'csharp']
+  const [language, setLanguage] = useState('javascript')
+  const [prettyPrint, setPrettyPrint] = useState(false)
+
+  const cycleLanguage = () => {
+    const currentIndex = languages.indexOf(language)
+    const nextIndex = (currentIndex + 1) % languages.length
+    setLanguage(languages[nextIndex])
+  }
+
+  const togglePrettyPrint = () => {
+    setPrettyPrint(!prettyPrint)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <PrettyPrintToggle enabled={prettyPrint} onToggle={togglePrettyPrint} />
+      <p>hi! i'm ayman fouad. i like to build things using {' '}<button onClick={cycleLanguage}>{language}</button></p>
+      <pre>{codeSnippets[language]}</pre>
+    </div>
   )
 }
 
